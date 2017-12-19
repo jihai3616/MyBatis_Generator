57c7b3716f08eacbaf804ce18733bb6be8fb2f49
zoubingbing <zoubingbing1991@foxmail.com>
2017-12-19 17:41:22 +0800

    mybatis-generation更新<table>字段

---
+++ src/main/java/com/zoubb/dao/TbOrderMapper.java
@@ -0,0 +1,30 @@
+package com.zoubb.dao;
+
+import com.zoubb.domain.TbOrder;
+import com.zoubb.domain.TbOrderExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbOrderMapper {
+    long countByExample(TbOrderExample example);
+
+    int deleteByExample(TbOrderExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    List<TbOrder> selectByExample(TbOrderExample example);
+
+    TbOrder selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByExample(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByPrimaryKeySelective(TbOrder record);
+
+    int updateByPrimaryKey(TbOrder record);
+}
---
+++ src/main/java/com/zoubb/domain/TbOrder.java
@@ -0,0 +1,45 @@
+package com.zoubb.domain;
+
+import java.util.Date;
+
+public class TbOrder {
+    private Integer id;
+
+    private String phone;
+
+    private Date ctime;
+
+    private Date utime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public Date getCtime() {
+        return ctime;
+    }
+
+    public void setCtime(Date ctime) {
+        this.ctime = ctime;
+    }
+
+    public Date getUtime() {
+        return utime;
+    }
+
+    public void setUtime(Date utime) {
+        this.utime = utime;
+    }
+}
